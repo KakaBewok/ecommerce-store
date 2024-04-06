17ce0e9b246e09b7cd1fd50e6dcb85d1a39f92fc
noprizal <noprizal@voltras.co.id>
2024-04-06 19:33:04 +0700

    fixing product card

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,14 +1,38 @@
 "use client";
 
 import { Product } from "@/types";
-import { ShoppingCart } from "lucide-react";
+import { Expand, ShoppingCart } from "lucide-react";
 import Image from "next/image";
+import IconButton from "@/components/ui/icon-button";
+import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import Currency from "@/components/ui/currency";
 
 interface ProductCard {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  // const previewModal = usePreviewModal();
+  // const cart = useCart();
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    // previewModal.onOpen(data);
+  };
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    // cart.addItem(data);
+  };
+
   return (
     <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/* Image & actions */}
@@ -21,15 +45,14 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
-            {/* <IconButton 
-                  onClick={onPreview} 
-                  icon={<Expand size={20} className="text-gray-600" />}
-                />
-                <IconButton
-                  onClick={onAddToCart} 
-                  icon={<ShoppingCart size={20} className="text-gray-600" />} 
-                /> */}
-            test
+            <IconButton
+              onClick={onPreview}
+              icon={<Expand size={20} className="text-gray-600" />}
+            />
+            <IconButton
+              onClick={onAddToCart}
+              icon={<ShoppingCart size={20} className="text-gray-600" />}
+            />
           </div>
         </div>
       </div>
@@ -40,7 +63,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
       </div>
       {/* Price & Review */}
       <div className="flex items-center justify-between">
-        {/* <Currency value={data?.price} /> */}
+        <Currency value={data?.price} />
       </div>
     </div>
   );
