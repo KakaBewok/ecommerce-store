a40d3fad0e1a8e47e52d443847fa757f446d1cda
noprizal <noprizal@voltras.co.id>
2024-04-07 14:36:42 +0700

    create product detail page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -4,6 +4,8 @@ import ProductList from "@/components/product-list";
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
 import Container from "@/components/ui/container";
+import Info from "@/components/info";
+import Gallery from "@/components/gallery";
 
 export const revalidate = 0;
 
@@ -28,10 +30,9 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            {/* <Gallery images={product.images} /> */}
+            <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-              {params.productId}
-              {/* <Info data={product} /> */}
+              <Info data={product} />
             </div>
           </div>
           <hr className="my-10" />
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import Button from "@/components/ui/Button";
+import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 import { ShoppingBag } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -13,7 +14,7 @@ const NavbarActions = () => {
   }, []);
 
   const router = useRouter();
-  // const cart = useCart();
+  const cart = useCart();
 
   if (!isMounted) {
     return null;
@@ -21,51 +22,17 @@ const NavbarActions = () => {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <div>navbar-action</div>
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button
+        onClick={() => alert("to cart page")}
+        className="flex items-center rounded-full bg-black px-4 py-2"
+      >
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
 };
 
 export default NavbarActions;
-
-// import { ShoppingBag } from "lucide-react";
-// import { useRouter } from "next/navigation";
-// import { useEffect, useState } from "react";
-
-// import Button from "@/components/ui/button";
-// import useCart from "@/hooks/use-cart";
-
-// const NavbarActions = () => {
-//   const [isMounted, setIsMounted] = useState(false);
-
-//   useEffect(() => {
-//     setIsMounted(true);
-//   }, []);
-
-//   const router = useRouter();
-//   const cart = useCart();
-
-//   if (!isMounted) {
-//     return null;
-//   }
-
-//   return (
-//     <div className="ml-auto flex items-center gap-x-4">
-//       <Button
-//         onClick={() => router.push("/cart")}
-//         className="flex items-center rounded-full bg-black px-4 py-2"
-//       >
-//         <ShoppingBag size={20} color="white" />
-//         <span className="ml-2 text-sm font-medium text-white">
-//           {cart.items.length}
-//         </span>
-//       </Button>
-//     </div>
-//   );
-// };
-
-// export default NavbarActions;
