410865a38e7042638b251c09b07167b4e8812447
noprizal <noprizal@voltras.co.id>
2024-04-19 00:08:20 +0700

    fixing footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Container from "./ui/container";
 
 const Footer = () => {
@@ -5,7 +6,9 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2024 fakeStoreName, inc. All rights reserve
+          &copy; 2024{" "}
+          <Link href={"https://www.instagram.com/_kkbwk"}>KakaBewok</Link>. All
+          rights reserve
         </p>
       </div>
     </footer>
